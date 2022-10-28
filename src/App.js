dae1214c2626a9138b0efbe730226d7c2c8cb859
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2022-10-28 23:22:09 +0530

    axios

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import "./App.css";
-import Child1 from "./practise/Child1";
-import Child2 from "./practise/Child2";
+// import Child1 from "./practise/Child1";
+// import Child2 from "./practise/Child2";
 import { useState } from "react";
+import AxiosDemo from "./practise/AxiosDemo";
 // import RenderingLists from "./practise/RenderingLists";
 // import StateButton from "./practise/StateButton";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const parentCount = (value) => {
-    setCount(value);
-  };
+  // const [count, setCount] = useState(0);
+  // const parentCount = (value) => {
+  //   setCount(value);
+  // };
   return (
     <>
-      <Child1 parentCount={parentCount} />
-      <Child2 count={count} />
+      <AxiosDemo />
+      {/* <Child1 parentCount={parentCount} />
+      <Child2 count={count} /> */}
     </>
   );
   // <StateButton />;
